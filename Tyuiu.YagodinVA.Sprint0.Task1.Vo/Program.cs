b3f02ee0190465d822596885db4aa1952ea2638a
Tyuiu.Yagodinv@outlook.com <Tyuiu.Yagodinv@outlook.com>
2023-09-12 13:15:39 +0300

    Task1

---
+++ Tyuiu.YagodinVA.Sprint0.Task1.Vo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.YagodinVA.Sprint0.Task1.Vo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            String name = "Владислав";
+            int age = 18;
+            Console.WriteLine("Здравствуйте меня зовут" + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
